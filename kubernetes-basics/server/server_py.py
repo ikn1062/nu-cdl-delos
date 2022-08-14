70d9bfb2c15a0a75588ff68bee307099626231c0
Ishaan Narain <ishaannarain2022@u.northwestern.edu>
2022-08-13 23:03:45 -0500

    Changing server py to have a stack for returning messages

--- kubernetes-basics/server/server_py.py
+++ kubernetes-basics/server/server_py.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import time
 
-PORT = 5051
+PORT = 5050
 # SERVER = "0.0.0.0"
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
@@ -13,38 +13,39 @@ FORMAT = "utf-8"
 DISCONNECT_MSG = "0"
 RETURN_MSG = "I got it, roger"
 
-connection_status = False
+message_stack = []
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 
 def handle_client(connection, client_addr):
     print(f"[NEW CONNECTION] {client_addr} connected...")
-    global connection_status
-    connection_status = True
+    global message_stack
     connected = True
     while connected:
         message = connection.recv(MSG_LENGTH).decode(FORMAT)
         if message:
             print(f"[{client_addr}] {message}")
+            return_msg = RETURN_MSG.encode(FORMAT)
+            print(f"[SERVER] returning Message {RETURN_MSG}")
+            message_stack.append(return_msg)
         else:
             print(f"[DISCONNECT] {client_addr} disconnecting...")
-            connection_status = False
+            message_stack = []
             connected = False
     connection.close()
 
 
 def send_return(connection):
-    return_msg = RETURN_MSG.encode(FORMAT)
     while True:
         time.sleep(MSG_WAIT)
-        if connection_status:
-            connection.send(return_msg)
+        if len(message_stack) > 0:
+            connection.send(message_stack.pop(0))
 
 
 def server_main():
     server.listen()
-    global connection_status
+    global message_stack
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         connection, client_addr = server.accept()
@@ -57,7 +58,7 @@ def server_main():
                 thread_send_return.start()
         except ConnectionResetError or OSError:
             print(f"[CONNECTION REFUSED] disconnected to client")
-            connection_status = False
+            message_stack = []
             thread_handle_client.join()
             thread_send_return.join()
             connection.close()
