0daccf7862f1c6b7e30573fa657fb2f9113c9ddc
Ishaan Narain <ishaannarain2022@u.northwestern.edu>
2022-08-13 23:02:50 -0500

    Added python files for client and server

---
+++ kubernetes-basics/client/client_py.py
@@ -0,0 +1,69 @@
+import socket
+import time
+import threading
+
+PORT = 5051
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+MSG_WAIT = (5 + 5)//2
+
+MSG_LENGTH = 64
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "0"
+
+
+def connect():
+    timer = 0
+    not_connected = True
+    client = None
+    while not_connected:
+        if timer > 0:
+            time.sleep(5)
+        timer += 1
+
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client.connect(ADDR)
+            not_connected = False
+        except ConnectionRefusedError as e:
+            print(f"[CONNECTION REFUSED] cannot connect to server at {SERVER}")
+            continue
+    return client
+
+
+def send(client, msg):
+    while True:
+        time.sleep(MSG_WAIT)
+        message = msg.encode(FORMAT)
+        client.send(message)
+
+
+def server_reply(client):
+    while True:
+        server_msg = ""
+        amount_received = 0
+        while not amount_received:
+            server_msg = client.recv(64)
+            amount_received += len(server_msg)
+        print(f"[SERVER] {server_msg.decode(FORMAT)}")
+
+
+def main():
+    while True:
+        client = connect()
+        client_msg = "Hello World!"
+        try:
+            # send(client, client_msg)
+            if threading.activeCount() < 2:
+                thread_send = threading.Thread(target=send, args=(client, client_msg))
+                thread_server_reply = threading.Thread(target=server_reply, args=(client,))
+                thread_send.start()
+                thread_server_reply.start()
+        except ConnectionResetError as e:
+            print("[CONNECTION REFUSED] disconnected to server")
+            thread_send.join()
+            thread_server_reply.join()
+            client.close()
+
+
+main()
---
+++ kubernetes-basics/server/server_py.py
@@ -0,0 +1,68 @@
+import socket
+import threading
+import time
+
+PORT = 5051
+# SERVER = "0.0.0.0"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+MSG_WAIT = (1 + 1)//2
+
+MSG_LENGTH = 64
+FORMAT = "utf-8"
+DISCONNECT_MSG = "0"
+RETURN_MSG = "I got it, roger"
+
+connection_status = False
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(connection, client_addr):
+    print(f"[NEW CONNECTION] {client_addr} connected...")
+    global connection_status
+    connection_status = True
+    connected = True
+    while connected:
+        message = connection.recv(MSG_LENGTH).decode(FORMAT)
+        if message:
+            print(f"[{client_addr}] {message}")
+        else:
+            print(f"[DISCONNECT] {client_addr} disconnecting...")
+            connection_status = False
+            connected = False
+    connection.close()
+
+
+def send_return(connection):
+    return_msg = RETURN_MSG.encode(FORMAT)
+    while True:
+        time.sleep(MSG_WAIT)
+        if connection_status:
+            connection.send(return_msg)
+
+
+def server_main():
+    server.listen()
+    global connection_status
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        connection, client_addr = server.accept()
+        try:
+            # handle_client(connection, client_addr)
+            if threading.activeCount() < 2:
+                thread_handle_client = threading.Thread(target=handle_client, args=(connection, client_addr))
+                thread_send_return = threading.Thread(target=send_return, args=(connection,))
+                thread_handle_client.start()
+                thread_send_return.start()
+        except ConnectionResetError or OSError:
+            print(f"[CONNECTION REFUSED] disconnected to client")
+            connection_status = False
+            thread_handle_client.join()
+            thread_send_return.join()
+            connection.close()
+            continue
+
+
+print("[STARTING] server is starting...")
+server_main()
