b16252176dd39927b1089a9d29a86760a987ec66
Ishaan Narain <ishaannarain2022@u.northwestern.edu>
2022-08-17 17:11:30 -0500

    Fixed mongodb bugs

--- kubernetes-mongodb/mongopy.py
+++ kubernetes-mongodb/mongopy.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
 import os
 
-MONGO_SERVER: os.getenv('MONGO_SERVER')
-MONGO_USER: os.getenv('MONGO_USERNAME')
-MONGO_PASS: os.getenv('MONGO_PASS')
-print(MONGO_SERVER)
-connect = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@{MONGO_SERVER}:27017/admin?ssl=true"
+MONGO_SERVER = os.getenv('MONGO_SERVER')
+MONGO_PORT = os.getenv('MONGO_PORT')
+MONGO_USER = os.getenv('MONGO_USERNAME')
+MONGO_PASS = os.getenv('MONGO_PASS')
+connect = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_SERVER}:{MONGO_PORT}"
 client = MongoClient(connect)
 print("CONNECTED!")
+print(client.list_database_names())
 client.close()
