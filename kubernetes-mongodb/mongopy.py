b63f25130d74d45723e6b1dda79b3d318383714c
Ishaan Narain <ishaannarain2022@u.northwestern.edu>
2022-08-17 17:46:29 -0500

    Updated python file for records

--- kubernetes-mongodb/mongopy.py
+++ kubernetes-mongodb/mongopy.py
@@ -1,12 +1,48 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 import os
 
 MONGO_SERVER = os.getenv('MONGO_SERVER')
 MONGO_PORT = os.getenv('MONGO_PORT')
 MONGO_USER = os.getenv('MONGO_USERNAME')
 MONGO_PASS = os.getenv('MONGO_PASS')
-connect = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_SERVER}:{MONGO_PORT}"
-client = MongoClient(connect)
-print("CONNECTED!")
-print(client.list_database_names())
-client.close()
+
+
+def mongo_connect():
+    try:
+        mongodb_uri = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_SERVER}:{MONGO_PORT}"
+        client = MongoClient(mongodb_uri)
+        print("CONNECTED!")
+        return client
+    except errors.ConnectionFailure as e:
+        print(f"[Error] Could not connect, error: {e}")
+        client.close()
+        return None
+
+
+def insert(collection, record):
+    if type(record) == list:
+        collection.insert_many(record)
+    else:
+        collection.insert_one(record)
+
+
+if __name__ == "__main__":
+    client = mongo_connect()
+    if client:
+        mydb = client["k8s_database"]
+        mycol = mydb["k8s_records"]
+        record1 = {"name": "Steve", "programming_language": "C"}
+        record2 = {"name": "Frank", "programming_language": "C++"}
+        record34 = [{"name": "John", "programming_language": "Python"},
+                    {"name": "Kevin", "programming_language": "Java"}]
+        insert(mycol, record1)
+        insert(mycol, record2)
+        for r in mycol.find():
+            print(r)
+        insert(mycol, record34)
+        for r in mycol.find():
+            print(r)
+        client.close()
+    else:
+        pass
+
